7aabb9bf82e66d7df825df54ca6b4fee07856d29
Linger <lingangliang3@gmail.com>
2022-10-19 16:27:25 +0800

    On 2022/10/19

---
+++ cudafft/complex.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <complex.h>
+using namespace std;
+
+complex<double> test(complex<double> t1, complex<double> t2) {
+    return t1 * t2;
+}
---
+++ img2gray/img2gray.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+
+int main() {
+  cv::Mat img_raw = cv::imread("raw.jpg", cv::IMREAD_GRAYSCALE);
+  cv::Mat img_out;
+  img_out.create(img_raw.size(), img_raw.type());
+
+  for (int i = 0; i < img_raw.rows; i++)
+    for (int j = 0; j < img_raw.cols; j++) {
+      uint8_t data = img_raw.at<uint8_t>(i, j);
+      if (data < 0xf0)
+        img_out.at<uint8_t>(i, j) = 0;
+      else
+        img_out.at<uint8_t>(i, j) = data;
+    }
+
+  cv::imwrite("raw_1.jpg", img_out);
+}
